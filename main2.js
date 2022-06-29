9aafa60d3899ce0c55c4dbd7ab5d862b8667bbb4
EC2 Default User <ec2-user@ip-172-31-92-117.ec2.internal>
2022-06-29 08:13:11 +0000

    電卓

--- main2.js
+++ main2.js
@@ -1,46 +1,64 @@
 /* global $ */
-function clickButton(button){
-  
-  let val = button.innerHTML;
-  let result = document.getElementById("result");
-  console.log(result.innerHTML);
-  
-  // 条件　計算済み/演算子
-  var finish = false;
-  var operator = false;
- 
-  if (val == "AC"){
-    result.innerHTML = "0";
-    
-  }else if (val == "="){
-    result.innerHTML = eval(result.innerHTML);
-    finish = true;
-  
-  // 演算子
-  }else if (val == "+" || "-" || "*" || "/" && operator == false){
-    result.innerHTML = result.innerHTML.slice(-1) + val;
-    operator = false;
-  }else if (val == "+" || "-" || "*" || "/" ){
+
+var result = document.getElementById("result");
+
+// 計算済み,演算子,小数点
+var finish = false;
+var operator = false;
+var point = true;
+
+function clickC(button){
+  var val = button.innerHTML;
+  result.innerHTML = "0";
+  point = true;
+};
+
+function clickE(button){
+  var val = button.innerHTML;
+  result.innerHTML = eval(result.innerHTML);
+  finish = true;
+  point = true;
+  operator = false;
+  if (result.innerHTML.last == clickO.innerHTML){
+    result.innerHTML.slice(0,-1);
+  }
+};
+
+function clickO(button){
+  var val = button.innerHTML;
+  if (operator == true){
     result.innerHTML += val;
-    operator = false;
-  
-  // 数字
-  }else if (finish == true){
-    result.innerHTML = val;
+  }else if (operator == false && result.innerHTML.last == clickO.innerHTML){
+    result.innerHTML = result.innerHTML.slice(0,-1) + val;
+  }
+  point = true;
+  operator = false;
+};
+
+function clickN(button){
+  var val = button.innerHTML;
+  if (finish == true){
     finish = false;
-    operator = true;
+    result.innerHTML = val;
   }else if (result.innerHTML == "0" && val == "0"){
     result.innerHTML = "0";
-    operator = true;
-  }else if (result.innerHTML == "0" && val == "."){
-    result.innerHTML = "0.";
-    operator = true;
+  }else if (result.innerHTML == "0" && val == "00"){
+    result.innerHTML = "0";
   }else if (result.innerHTML == "0"){
     result.innerHTML = val;
-    operator = true;
   }else {
     result.innerHTML += val;
-    operator = true;
   }
+  operator = true;
+};
   
+  function clickP(button){
+    var val = button.innerHTML;
+    if (finish == true){
+      result.innerHTML = "0.";
+    }else if (point == true){
+      result.innerHTML += val;
+    }
+    point = false;
+    finish = false;
 };
